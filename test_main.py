752b0280245f58c9a18379d67c7c2cb7048aee5c
Joe Chea <78744570+joechea-aupp@users.noreply.github.com>
2023-05-16 11:40:12 +0700

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,28 @@
+import unittest
+from main import currency_conversion
+
+class CodingRoomsUnitTests(unittest.TestCase):
+    def setUp(self):
+        # Setup code here (if required, replace the 'pass')
+        pass
+
+    def tearDown(self):
+        # Teardown code here (if required, replace the 'pass')
+        pass
+
+    def test_default_case(self):
+        # Your test case logic here (replace the example assertion below)
+        # You may also rename this to any function in the form of 'test_your_test_name(self):'
+        self.assertTrue(True)
+
+    def test_current_conversion(self):
+        self.assertEqual(currency_conversion('USD', 2), 8000)
+        self.assertEqual(currency_conversion('usd', 3), 12000)
+        self.assertEqual(currency_conversion('Yuan', 3), 1929)
+        self.assertEqual(currency_conversion('batH', 2), 246)
+        self.assertEqual(currency_conversion('Yen', 2), 'not found')
+        self.assertEqual(currency_conversion('yuan', 'ab'), 'invalid amount')
+
+if __name__ == '__main__':
+    unittest.main()
+
